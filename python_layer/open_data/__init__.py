151d13ca3c9524f8f4d22c67cd1363b0f47e9674
Muhoza Ursus Bizumuremyi <62938563+ursus123@users.noreply.github.com>
2026-03-24 10:42:36 -0400

    Update __init__.py

--- python_layer/open_data/__init__.py
+++ python_layer/open_data/__init__.py
@@ -1,1 +1,23 @@
-# open_data/__init__.py
+from open_data.medication_routes   import router as medication_router
+from open_data.healthcare_routes   import router as healthcare_router
+from open_data.labor_routes        import router as labor_router
+from open_data.demographics_routes import router as demographics_router
+from open_data.geospatial_routes   import router as geospatial_router
+from open_data.education_routes    import router as education_router
+from open_data.agriculture_routes  import router as agriculture_router
+from open_data.nonprofit_routes    import router as nonprofit_router
+from open_data.market_routes       import router as market_router
+
+ALL_ROUTERS = [
+    medication_router,
+    healthcare_router,
+    labor_router,
+    demographics_router,
+    geospatial_router,
+    education_router,
+    agriculture_router,
+    nonprofit_router,
+    market_router,
+]
+
+__all__ = ["ALL_ROUTERS"]
