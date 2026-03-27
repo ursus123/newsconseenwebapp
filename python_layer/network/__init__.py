e47e7cb2b4732b422edeedc4a5d1b0425d053cd1
Muhoza Ursus Bizumuremyi <62938563+ursus123@users.noreply.github.com>
2026-03-27 18:08:54 +0000

    Update __init__.py

--- python_layer/network/__init__.py
+++ python_layer/network/__init__.py
@@ -2,5 +2,6 @@
 from network.registry import NetworkRegistry
 from network.aggregator import NetworkAggregator
 from network.routes import router as network_router
+from network.benchmarks import NetworkBenchmarks
 
-__all__ = ["NetworkRegistry", "NetworkAggregator", "network_router"]
+__all__ = ["NetworkRegistry", "NetworkAggregator", "network_router","NetworkBenchmarks"]
