ce1aadbccfae5f432e1ea470c71a0ba48ff6a9c9
Muhoza Ursus Bizumuremyi <62938563+ursus123@users.noreply.github.com>
2026-03-27 16:59:31 +0000

    Create __init__.py

---
+++ python_layer/copilot/__init__.py
@@ -0,0 +1,6 @@
+# Newsconseen Operational Copilot
+from copilot.engine import CopilotEngine
+from copilot.queries import QueryEngine
+from copilot.routes import router as copilot_router
+
+__all__ = ["CopilotEngine", "QueryEngine", "copilot_router"]
